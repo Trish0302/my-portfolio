819cb8b733a700827645b84afaa3e61968560f02
Trish0302 <letri03022001@gmail.com>
2022-01-26 12:05:24 +0700

    fifth

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ let menu = document.querySelector('.header #menu');
 let navbar = document.querySelector('.header nav');
 let copyRight = document.querySelector('.header nav .copy-right');
 
-menu.onclick = function() {
+menu.onclick = function () {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
     copyRight.classList.toggle('active');
 }
+
+$(document).ready(function() {
+    console.log('dsd hello');
+    $('.grid').colcade({
+        columns: '.grid-col',
+        items: '.grid-item'
+    })
+})
